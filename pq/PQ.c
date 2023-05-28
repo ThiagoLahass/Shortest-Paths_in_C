5ca6ca86755a7b7a820a14d1450a6455d8e74018
thamyadonadia <thamya.donadia@edu.ufes.br>
2023-05-28 19:25:34 -0300

    :recycle: removed global variables

--- pq/PQ.c
+++ pq/PQ.c
@@ -2,79 +2,106 @@
 #include <stdlib.h>
 #include "PQ.h"
 
-static Item *pq;
-static int *map;
-static int N;
+struct pq{
+    Item* v;
+    int* map;
+    int N;
+};
 
-static void swap(int i, int j) {
-    exch(pq[i], pq[j]);
-    map[id(pq[i])] = i;
-    map[id(pq[j])] = j;
+static void swap(PQ* f, int i, int j) {
+    exch(f->v[i], f->v[j]);
+    f->map[id(f->v[i])] = i;
+    f-> map[id(f->v[j])] = j;
 }
 
-void fix_up(Item *a, int k) {
-    while (k > 1 && more(a[k/2], a[k])) {
-        swap(k, k/2);
+void fix_up(PQ* f, int k) {
+    while (k > 1 && more(f->v[k/2], f->v[k])) {
+        swap(f, k, k/2);
         k = k/2;
     }
 }
 
-void fix_down(Item *a, int sz, int k){
+void fix_down(PQ* f, int sz, int k){
   while (2*k <= sz) {
     int j = 2*k;
-    if (j < sz && more(a[j], a[j+1])){
+    if (j < sz && more(f->v[j], f->v[j+1])){
       j++;
     }
-    if (!more(a[k], a[j])) {
+    if (!more(f->v[k], f->v[j])) {
       break;
     }
-    swap(k, j);
+    swap(f,k, j);
     k = j;
   }
 }
 
-void PQ_init(int maxN) {
-    pq = (Item *) malloc((maxN+1) * sizeof (Item));
-    map = (int *) malloc((maxN+1) * sizeof (int));
-    N = 0;
+PQ* PQ_init(int maxN) {
+    PQ* nova = malloc(sizeof(PQ));    
+    nova->v = (Item *) malloc((maxN+1) * sizeof (Item));
+    nova->map = (int *) malloc((maxN+1) * sizeof (int));
+    nova->N = 0;
+    return nova;
 }
 
-void PQ_insert(Item v) {
-    N++;
-    pq[N] = v;
-    map[id(v)] = N;
-    fix_up(pq, N);
+void PQ_insert(PQ* f, int id, double value) {
+    f->N++;
+    Item novo; novo.id = id; novo.value = value; 
+    
+    f->v[f->N] = novo;
+    f->map[id(novo)] = f->N;
+    fix_up(f, f->N);
 }
 
-Item PQ_delmin() {
-    Item min = pq[1];
-    swap(1, N);
-    N--;
-    fix_down(pq, N, 1);
-    return min;
+int PQ_delmin(PQ* f) {
+    Item min = f->v[1];
+    swap(f, 1, f->N);
+    f->N--;
+    fix_down(f, f->N, 1);
+    return min.id;
 }
 
-Item PQ_min() {
-    return pq[1];
+Item PQ_min(PQ* f) {
+    return f->v[1];
 }
 
-void PQ_decrease_key(int id, double value) {
-    int i = map[id];
-    value(pq[i]) = value;
-    fix_up(pq, i);
+void PQ_decrease_key(PQ* f, int id, double value) {
+    int i = f->map[id];
+    value(f->v[i]) = value;
+    fix_up(f->v, i);
 }
 
-bool PQ_empty() {
-    return N == 0;
+bool PQ_empty(PQ* f) {
+    return f->N == 0;
 }
 
-int  PQ_size() {
-    return N;
+int  PQ_size(PQ* f) {
+    return f->N;
 }
 
-void PQ_finish() {
-    free(pq);
-    free(map);
+bool PQ_contains(PQ* f, int id){
+    for(int i=0; i<f->N; i++) if(id(f->v[i]) == id) return true;
+    return false;
+}
+
+void PQ_finish(PQ* f) {
+    free(f->v);
+    free(f->map);
+    free(f);
+}
+
+void PQ_imprime(PQ* f){
+    for(int i=0; i<f->N; i++){
+        printf("%d ", f->v[i].id+1);
+    }
+
+    printf("\n");
+
+    for(int i=0; i<f->N; i++){
+        printf("%d ", f->v[i].value);
+    }
+
+    printf("\n\n");
+
 }
 
 
--- pq/PQ.h
+++ pq/PQ.h
@@ -4,13 +4,18 @@
 #include <stdbool.h>
 #include "item.h"
 
-void PQ_init(int maxN); // cria uma min PQ com capacidade maxN
-void PQ_insert(Item);   // Insere Item na PQ. A maior prioridade é a do Item com menor campo 'value'
-Item PQ_delmin();       // Remove Item com menor campo 'value' da PQ
-void PQ_decrease_key(int id, double value); // Muda a prioridade do nó com identificador 'id' para 'value'
-Item PQ_min();          // Retorna Item com menor campo 'value' da PQ
-bool PQ_empty();        // Retorna True se a PQ não tem elementos
-int  PQ_size();         // Número de elementos na PQ
-void PQ_finish();       // Libera memória
+typedef struct pq PQ;
+
+PQ* PQ_init(int maxN); // cria uma min PQ com capacidade maxN
+void PQ_insert(PQ* f, int id, double value); // Insere Vertice na PQ. A maior prioridade é a do Vertice com menor campo 'value'
+int PQ_delmin(PQ* f);     // Remove Vertice com menor campo 'value' da PQ
+void PQ_decrease_key(PQ* f, int id, double value); // Muda a prioridade do nó com identificador 'id' para 'value'
+Item PQ_min(PQ* f);      // Retorna Vertice com menor campo 'value' da PQ
+bool PQ_empty(PQ* f);        // Retorna True se a PQ não tem elementos
+int  PQ_size(PQ* f);         // Número de elementos na PQ
+bool PQ_contains(PQ* f, int id);
+void PQ_finish(PQ* f);       // Libera memória
+
+void PQ_imprime(PQ* f);
 
 #endif
