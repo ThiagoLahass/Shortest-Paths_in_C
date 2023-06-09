9cab3d9d2279b4de0254ce518d28abd4c583e91f
thamyadonadia <thamya.donadia@edu.ufes.br>
2023-06-09 12:12:44 -0300

    :art: improved format of the code

--- aresta.c
+++ aresta.c
@@ -7,75 +7,6 @@ struct aresta{
     double velocidade;
 };
 
-/*int aresta_retornaOrigem(Aresta *aresta, int posicao){
-    return aresta[posicao].origem;
-}
-int aresta_retornaDestino(Aresta *aresta, int posicao){
-    return aresta[posicao].destino;
-}
-double aresta_retornaDistancia(Aresta *aresta, int posicao){
-    return aresta[posicao].distancia;
-}
-double aresta_retornaVelocidade(Aresta *aresta, int posicao){
-    return aresta[posicao].velocidade;
-}*/
-
-
-int aresta_retornaOrigem(Aresta* aresta){
-    return aresta->origem;
-}
-int aresta_retornaDestino(Aresta* aresta){
-    return aresta->destino;
-}
-double aresta_retornaDistancia(Aresta* aresta){
-    return aresta->distancia;
-}
-double aresta_retornaVelocidade(Aresta* aresta){
-    return aresta->velocidade;
-}
-double aresta_retornaTempoPercurso(Aresta* aresta){
-    double velocidade_m_s = aresta->velocidade/3.6;
-    return aresta->distancia/velocidade_m_s;
-}
-
-void aresta_atualizaVelocidade( Aresta* a, double velocidade ){
-    a->velocidade = velocidade;
-}
-
-Aresta* aresta_LeArquivo(FILE *file, int quantidadeArestas, double velocidadeInicial){
-    Aresta* lista_aresta = malloc(quantidadeArestas * sizeof(Aresta));    //lista struct aresta
-
-    char *line = malloc(50*sizeof(char)); //recebe todos os dados de uma linha
-    size_t len =50;                       //somente usado na função getline
-
-    for(int i=0; i<quantidadeArestas && ((getline(&line, &len, file)) != -1); i++){
-        lista_aresta[i].origem     = atoi(strtok(line, ";")); 
-        lista_aresta[i].destino    = atoi(strtok(NULL, ";"));
-        lista_aresta[i].distancia  = atof(strtok(NULL, ";"));
-        lista_aresta[i].velocidade = velocidadeInicial;
-    }
-
-    free(line);
-    return lista_aresta;
-}
-
-
-void aresta_exibe(Aresta *aresta, int quantidadeArestas){
-    for(int i=0; i<quantidadeArestas; i++){
-        printf("aresta[%d]:  origem:%d  dest:%d  ", i, aresta[i].origem, aresta[i].destino);
-        printf("Dist:%.2lf  Veloc:%0.2lf\n", aresta[i].distancia, aresta[i].velocidade);
-    }
-    return;
-}
-
-void aresta_imprime(Aresta* a){
-    printf("<(%d,%d) d = %f, v = %f > ", a->origem, a->destino, a->distancia, a->velocidade);
-}
-
-void aresta_free(Aresta *arestas){
-    free(arestas);
-}
-
 Aresta* aresta_constroi(int origem, int destino, double distancia, double velocidade){
     Aresta* nova = malloc(sizeof(Aresta));
 
@@ -85,4 +16,33 @@ Aresta* aresta_constroi(int origem, int destino, double distancia, double veloci
     nova->velocidade = velocidade;
 
     return nova;
+}
+
+int aresta_retornaOrigem(Aresta* a){
+    return a->origem;
+}
+
+int aresta_retornaDestino(Aresta* a){
+    return a->destino;
+}
+
+double aresta_retornaDistancia(Aresta* a){
+    return a->distancia;
+}
+
+double aresta_retornaVelocidade(Aresta* a){
+    return a->velocidade;
+}
+
+double aresta_retornaTempoPercurso(Aresta* a){
+    double velocidade_m_s = a->velocidade/3.6;
+    return a->distancia/velocidade_m_s;
+}
+
+void aresta_atualizaVelocidade(Aresta*a, double velocidade){
+    a->velocidade = velocidade;
+}
+
+void aresta_imprime(Aresta* a){
+    printf("<(%d,%d) d = %f, v = %f > ", a->origem, a->destino, a->distancia, a->velocidade);
 }
